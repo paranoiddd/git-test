f5fc60323472d701d78b6995637988a7407ce376
Calf <Calf@163.com>
2022-03-13 17:33:30 +0800

    git测试3

--- src/main/java/com/wyl/test/GitTest.java
+++ src/main/java/com/wyl/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git的测试");
         System.out.println("git的测试2");
+        System.out.println("git的测试3");
     }
 }
