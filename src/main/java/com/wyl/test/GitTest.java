f4bc5168fe0223539d0c24fa00cf6f51f2eb7c3d
Calf <Calf@163.com>
2022-03-13 17:49:01 +0800

    git测试hot-fix2

--- src/main/java/com/wyl/test/GitTest.java
+++ src/main/java/com/wyl/test/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git的测试");
         System.out.println("git的测试2");
-        System.out.println("git的测试3");
+        System.out.println("git的测试3-hot-fix");
         System.out.println("git的测试4-hot-fix");
     }
 }
